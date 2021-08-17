71ba4bb31c55b1d4862aeaf0ba084e539a4e38ad
Seungjoo Kim <sjskybone@gmail.com>
2021-08-17 16:54:56 -0700

    Set up for the firebase.

---
+++ business-card-maker/src/service/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: ProcessingInstruction.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: ProcessingInstruction.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: ProcessingInstruction.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: ProcessingInstruction.env.REACT_APP_FIREBASE_PROJECT_ID
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
