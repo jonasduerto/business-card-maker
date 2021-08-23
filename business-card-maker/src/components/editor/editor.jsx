2dbbea9214fcfb04dd7f229127c440282ee648a3
Seungjoo Kim <sjskybone@gmail.com>
2021-08-23 16:09:49 -0700

    Created add form.jsx. #5

--- business-card-maker/src/components/editor/editor.jsx
+++ business-card-maker/src/components/editor/editor.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CardAddForm from '../card-add-form/card-add-form';
 import CardEditForm from '../card-edit-form/card-edit-form';
 import styles from './editor.module.css';
 
@@ -7,8 +8,9 @@ const Editor = ({cards}) => {
     <section className={styles.editor}>
       <h1 className={styles.title}>Card Maker</h1>
       {cards.map(card => (
-        <CardEditForm card={card} />
+        <CardEditForm key={card.id} card={card} />
       ))}
+      <CardAddForm />
     </section>
   );
 };
