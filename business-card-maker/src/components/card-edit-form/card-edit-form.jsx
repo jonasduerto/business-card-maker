dd93346ae92ffd3d327c878c4babbd4e0eb60c1c
Seungjoo Kim <sjskybone@gmail.com>
2021-08-23 15:32:13 -0700

    Added image and delete button. #3

--- business-card-maker/src/components/card-edit-form/card-edit-form.jsx
+++ business-card-maker/src/components/card-edit-form/card-edit-form.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from '../button/button';
+import ImageFileInput from '../image_file_input/image_file_input';
 import styles from './card-edit-form.module.css';
 
 const CardEditForm = ({card}) => {
@@ -13,18 +15,24 @@ const CardEditForm = ({card}) => {
     fileURL
   } = card;
 
+  const onSubmit = () => {
+
+  };
+
   return (
-    <form>
-      <input type="text" name="name" value={name} />
-      <input type="text" name="company" value={company} />
-      <select name="theme" value={theme}>
+    <form className={styles.form}>
+      <input className={styles.input} type="text" name="name" value={name} />
+      <input className={styles.input} type="text" name="company" value={company} />
+      <select className={styles.select} name="theme" value={theme}>
         <option value="light">Light</option>
         <option value="dark">Dark</option>
         <option value="colorful">Colorful</option>
       </select>
-      <input type="text" name="title" value={title} />
-      <input type="text" name="email" value={email} />
-      <textarea name="message" value={message}></textarea>
+      <input className={styles.input} type="text" name="title" value={title} />
+      <input className={styles.input} type="text" name="email" value={email} />
+      <textarea className={styles.textarea} name="message" value={message}></textarea>
+      <ImageFileInput />
+      <Button name='Delete' onClick={onSubmit} />
     </form>
   );
 };
