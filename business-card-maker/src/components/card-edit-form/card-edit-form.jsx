40d7c8d168f61fc24358c88b4415f06924026983
Seungjoo Kim <sjskybone@gmail.com>
2021-08-23 15:16:30 -0700

    Added card maker edit form and button. #3

--- business-card-maker/src/components/card-edit-form/card-edit-form.jsx
+++ business-card-maker/src/components/card-edit-form/card-edit-form.jsx
@@ -1,6 +1,32 @@
 import React from 'react';
 import styles from './card-edit-form.module.css';
 
-const CardEditForm = ({card}) => <h1>{card.id}</h1>;
+const CardEditForm = ({card}) => {
+  const {
+    name,
+    company,
+    title,
+    email,
+    message,
+    theme,
+    fileName,
+    fileURL
+  } = card;
+
+  return (
+    <form>
+      <input type="text" name="name" value={name} />
+      <input type="text" name="company" value={company} />
+      <select name="theme" value={theme}>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        <option value="colorful">Colorful</option>
+      </select>
+      <input type="text" name="title" value={title} />
+      <input type="text" name="email" value={email} />
+      <textarea name="message" value={message}></textarea>
+    </form>
+  );
+};
 
 export default CardEditForm;
