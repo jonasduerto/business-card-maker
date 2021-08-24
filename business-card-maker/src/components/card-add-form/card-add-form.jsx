3299759dc46e864f10a37352a5cadcec9a6a4f72
Seungjoo Kim <sjskybone@gmail.com>
2021-08-24 11:07:47 -0700

    Added onSubmit and CardAddForm function. #5

--- business-card-maker/src/components/card-add-form/card-add-form.jsx
+++ business-card-maker/src/components/card-add-form/card-add-form.jsx
@@ -1,25 +1,83 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Button from '../button/button';
 import ImageFileInput from '../image_file_input/image_file_input';
 import styles from './card-add-form.module.css';
 
-const CardAddForm = () => {
-  const onSubmit = () => {
+const CardAddForm = ({onAdd}) => {
+  const formRef = useRef();
+  const nameRef = useRef();
+  const companyRef = useRef();
+  const themeRef = useRef();
+  const titleRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
 
+  const onSubmit = event => {
+    event.preventDefault();
+
+    const card = {
+      id: Date.now(),
+      name: nameRef.current.value || '',
+      company: companyRef.current.value || '',
+      theme: themeRef.current.value || '',
+      title: titleRef.current.value || '',
+      email: emailRef.current.value || '',
+      message: messageRef.current.value || '',
+      fileName: '',
+      fileURL: ''
+    }
+
+    formRef.current.reset();
+    onAdd(card);
   };
 
   return (
-    <form className={styles.form}>
-      <input className={styles.input} type="text" name="name" placeholder="Name" />
-      <input className={styles.input} type="text" name="company" placeholder="Company" />
-      <select className={styles.select} name="theme" placeholder="Theme">
+    <form ref={formRef} className={styles.form}>
+      <input
+        ref={nameRef}
+        className={styles.input}
+        type="text"
+        name="name"
+        placeholder="Name"
+      />
+      <input
+        ref={companyRef}
+        className={styles.input}
+        type="text"
+        name="company"
+        placeholder="Company"
+      />
+      <select
+        ref={themeRef}
+        className={styles.select}
+        name="theme"
+        placeholder="Theme"
+      >
         <option placeholder="light">Light</option>
         <option placeholder="dark">Dark</option>
         <option placeholder="colorful">Colorful</option>
       </select>
-      <input className={styles.input} type="text" name="title" placeholder="Title" />
-      <input className={styles.input} type="text" name="email" placeholder="Email" />
-      <textarea className={styles.textarea} name="message" placeholder="Message"></textarea>
+      <input
+        ref={titleRef}
+        className={styles.input}
+        type="text"
+        name="title"
+        placeholder="Title"
+      />
+      <input
+        ref={emailRef}
+        className={styles.input}
+        type="text"
+        name="email"
+        placeholder="Email"
+      />
+      <textarea
+        ref={messageRef}
+        className={styles.textarea}
+        name="message"
+        placeholder="Message"
+      />
+
       <div className={styles.fileInput}>
         <ImageFileInput />
       </div>
--- business-card-maker/src/components/editor/editor.jsx
+++ business-card-maker/src/components/editor/editor.jsx
@@ -3,14 +3,14 @@ import CardAddForm from '../card-add-form/card-add-form';
 import CardEditForm from '../card-edit-form/card-edit-form';
 import styles from './editor.module.css';
 
-const Editor = ({cards}) => {
+const Editor = ({cards, addCard}) => {
   return (
     <section className={styles.editor}>
       <h1 className={styles.title}>Card Maker</h1>
       {cards.map(card => (
         <CardEditForm key={card.id} card={card} />
       ))}
-      <CardAddForm />
+      <CardAddForm onAdd={addCard} />
     </section>
   );
 };
