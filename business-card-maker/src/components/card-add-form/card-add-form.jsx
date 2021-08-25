53f8efd7401512e0d0388d4cc13ce95c929ecc59
Seungjoo Kim <sjskybone@gmail.com>
2021-08-25 15:48:31 -0700

    Created FileInput and ImageUploader functions. #11

--- business-card-maker/src/components/card-add-form/card-add-form.jsx
+++ business-card-maker/src/components/card-add-form/card-add-form.jsx
@@ -1,9 +1,8 @@
 import React, { useRef } from 'react';
 import Button from '../button/button';
-import ImageFileInput from '../image_file_input/image_file_input';
 import styles from './card-add-form.module.css';
 
-const CardAddForm = ({onAdd}) => {
+const CardAddForm = ({FileInput, onAdd}) => {
   const formRef = useRef();
   const nameRef = useRef();
   const companyRef = useRef();
@@ -80,7 +79,7 @@ const CardAddForm = ({onAdd}) => {
       />
 
       <div className={styles.fileInput}>
-        <ImageFileInput />
+        <FileInput />
       </div>
       <Button name='Add' onClick={onSubmit} />
     </form>
--- business-card-maker/src/components/card-edit-form/card-edit-form.jsx
+++ business-card-maker/src/components/card-edit-form/card-edit-form.jsx
@@ -1,9 +1,8 @@
 import React, { useRef } from 'react';
 import Button from '../button/button';
-import ImageFileInput from '../image_file_input/image_file_input';
 import styles from './card-edit-form.module.css';
 
-const CardEditForm = ({card, updateCard, deleteCard}) => {
+const CardEditForm = ({FileInput, card, updateCard, deleteCard}) => {
   const nameRef = useRef();
   const companyRef = useRef();
   const themeRef = useRef();
@@ -92,7 +91,7 @@ const CardEditForm = ({card, updateCard, deleteCard}) => {
       />
 
       <div className={styles.fileInput}>
-        <ImageFileInput />
+        <FileInput />
       </div>
       <Button name='Delete' onClick={onSubmit} />
     </form>
