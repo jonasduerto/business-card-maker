fbcdf771660fd04da72f5d7b1b05a60e8dbd4aa7
Seungjoo Kim <sjskybone@gmail.com>
2021-08-25 16:15:42 -0700

    Created image input button withe onButtonClick event. #11

--- business-card-maker/src/components/image_file_input/image_file_input.jsx
+++ business-card-maker/src/components/image_file_input/image_file_input.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './image_file_input.module.css';
 
-const ImageFileInput = ({ImageUploader}) => <button>Image</button>;
+const ImageFileInput = ({ImageUploader, name, onFileChange}) => {
+  const inputRef = useRef();
+
+  const onButtonClick = () => {
+    inputRef.current.click();
+  };
+
+  return (
+    <div className={styles.container}>
+      <input
+        ref={inputRef}
+        className={styles.input}
+        type="file"
+        accept="image/*"
+        name="file"
+      />
+      <button className={styles.button} onClick={onButtonClick}>
+        {name || 'No file'}
+      </button>
+    </div>
+  );
+}
 
 export default ImageFileInput;
