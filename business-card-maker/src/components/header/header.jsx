187c34b8c2969e1fda14c81b9df4a40375b3cf26
Seungjoo Kim <sjskybone@gmail.com>
2022-03-09 18:41:24 -0800

    Checking.

--- business-card-maker/src/components/header/header.jsx
+++ business-card-maker/src/components/header/header.jsx
@@ -8,7 +8,11 @@ const Header = memo(({ onLogout }) => (
         Logout
       </button>
     )}
-    <img className={styles.logo} src="/images/logo.png" alt="logo" />
+    <img
+      className={styles.logo}
+      src={process.env.PUBLIC_URL + `images/logo.png`}
+      alt="logo"
+    />
     <h1 className={styles.title}>Business Card Maker</h1>
   </header>
 ));
